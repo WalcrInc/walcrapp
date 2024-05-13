7d07a189c7b2186fae550307cdf1af2def984e12
astroboy20 <tolulopeakinkunmi7@gmail.com>
2024-05-13 17:02:56 +0100

    update

--- assets/index.js
+++ assets/index.js
@@ -1088,8 +1088,8 @@ export const MessageIcon = () => (
 );
 export const BigPlusIcon = () => (
   <svg
-    width="50"
-    height="50"
+    width="100"
+    height="100"
     xmlns="http://www.w3.org/2000/svg"
     fill="#1a1a1a"
     viewBox="0 0 24 24"
