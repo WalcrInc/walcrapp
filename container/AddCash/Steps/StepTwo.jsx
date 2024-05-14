4d87e03c9148a051c2704545bc11a8e77876264f
astroboy20 <tolulopeakinkunmi7@gmail.com>
2024-05-14 18:31:14 +0100

    feat: update

--- container/AddCash/Steps/StepTwo.jsx
+++ container/AddCash/Steps/StepTwo.jsx
@@ -1,50 +1,62 @@
-import { DeleteIcon } from '@/assets'
-import { Button } from '@chakra-ui/react'
-import React from 'react'
+import { DeleteIcon } from "@/assets";
+import { Button, Input } from "@chakra-ui/react";
+import { useState } from "react";
 
-const StepTwo = ({input, handleBackspace, handleNumberInput}) => {
+const StepTwo = ({  handleBackspace }) => {
+  const [pin, setPin] = useState("");
+  const handleChange = (e) => {
+    setPin(e.target.value);
+  };
+  
+
+ 
+  const handleNumberInput = (number) => {
+    // Check if the pin length is already 4
+    if (pin?.length === 4) {
+      return; // Return early if the PIN is already 4 digits
+    }
+  
+    // Update the PIN with the new number
+    setPin((prevPin) => prevPin + number);
+  };
+  
+  
   return (
     <>
-    heyy
-    <div className="number-body">
+      <div>heyy</div>
+      <div>
+        <Input
+          value={pin}
+          onChange={handleChange}
+          readOnly
+        />
+      </div>
 
-<Button
-  className="button-submit"
-  color={"#fff"}
-  size={"lg"}
-  margin={"auto"}
-  background={"#1a1a1a"}
-  width={"90%"}
->
-  Add {input ? `${input}` : "$0.00"}
-</Button>
-<div className="numbers">
-  {[1, 2, 3, 4, 5, 6, 7, 8, 9, ".", 0].map((number) => (
-    <button
-      className="number-button"
-      key={number}
-      onClick={() => handleNumberInput(number)}
-    >
-      {number}
-    </button>
-  ))}
-  <button
-    style={{
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-    }}
-    onClick={handleBackspace}
-  >
-    <DeleteIcon/>
-  </button>
-</div>
-</div>
-
-    
+      <div className="number-body">
+        <div className="numbers">
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, ".", 0].map((number) => (
+            <button
+              className="number-button"
+              key={number}
+              onClick={() => handleNumberInput(number)}
+            >
+              {number}
+            </button>
+          ))}
+          <button
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+            onClick={handleBackspace}
+          >
+            <DeleteIcon />
+          </button>
+        </div>
+      </div>
     </>
-    
-  )
-}
+  );
+};
 
-export  {StepTwo}
+export { StepTwo };
