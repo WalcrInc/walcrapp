aaf99cdc6f03f9dad1ca26e928971646f2bbb9f6
astroboy20 <tolulopeakinkunmi7@gmail.com>
2024-05-27 17:09:52 +0100

    update

--- container/Home/Page.jsx
+++ container/Home/Page.jsx
@@ -1,31 +1,30 @@
 import { Button } from "@chakra-ui/react";
-import Image from "next/image";
 import React from "react";
 import { PageContainer } from "./Page.style";
-import { useRouter } from "next/router";
 import useRoutes from "@/hooks/Routes/Routes";
 import { LogoWhiteX } from "@/components/Logo/Logo";
 
 const Page = () => {
   const { handleOnboardingRoute } = useRoutes();
+
   return (
     <PageContainer>
       <header>
-        <LogoWhiteX /> 
-        <div className="overlay">
-      {/* <Image src={"/images/overlay.svg"} height={50} width={50} alt="heyy" /> */}
-      </div>
+        <div className="logo">
+          <LogoWhiteX /> 
+        </div>
+        <div className="overlay" />
       </header>
      
-
       <div className="text">
-        <h1> Lorem ipsum dolor amet</h1>
+        <h1>Lorem ipsum dolor amet</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         <Button
           onClick={handleOnboardingRoute}
           color={"#fff"}
           background={"#1a1a1a"}
           size={"lg"}
+          width={"100%"}
         >
           Get Started
         </Button>
--- container/Home/Page.style.js
+++ container/Home/Page.style.js
@@ -3,30 +3,45 @@ import styled from "styled-components";
 export const PageContainer = styled.div`
   display: flex;
   flex-direction: column;
-  overflow: hidden;
-  height:100% ;
+  height: 100vh; /* Full height of the viewport */
+
   header {
-    background: url("/images/home.svg");
-    height: 60dvh;
-    background-size:cover ;
-    background-repeat:no-repeat ;
-    /* background-position: 50% ; */
-    padding: 6%;
-    width:100% ;
+    background: url("/images/home.svg") no-repeat center center;
+    background-size: cover;
+    flex: 2; /* Take up two-thirds of the container height */
+    display: flex;
+    height: 100%;
+    width: 100%;
+    position: relative;
+
+    .logo {
+      position: absolute;
+      top: 0;
+      left: 0;
+      margin: 20px; /* Adjust as needed for spacing */
+    }
+
+    .overlay {
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 112px;
+      background: url("/images/overlay.svg") no-repeat center center;
+    }
   }
-  .overlay{
-    position:relative ;
-    bottom:20px ;
-    width:100% ;
-    height:112px ;
-  }
+
   .text {
+    flex: 1; /* Take up the other third of the container height */
     display: flex;
     flex-direction: column;
+    justify-content: center; /* Center vertically */
+    align-items: center; /* Center horizontally */
     gap: 30px;
     text-align: center;
     padding: 3%;
   }
+
   .text h1 {
     font-size: 3.4rem;
     font-weight: 800;
