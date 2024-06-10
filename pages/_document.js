266bf62ae8374692aa72fad386f07f4c6a00c054
astroboy20 <tolulopeakinkunmi7@gmail.com>
2024-06-10 15:31:44 +0100

    update

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ export default class MyDocument extends Document {
           <meta name="theme-color" content="#000000" />
           <meta name="mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
           <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover" />
         </Head>
         <body>
