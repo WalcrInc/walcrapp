a44da4cde10e5ff1079d0845625509794aeb8855
Walcr <walcr.com@gmail.com>
2024-07-03 13:27:02 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,8 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="manifest" href="/manifest.json" />
-          <meta name="theme-color" content="#ffffff" />
+          <link rel="stylesheet" href="styleovrride.css" /> 
+          <meta name="theme-color" content="#ffffffff" />
           <meta name="mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="default" />
