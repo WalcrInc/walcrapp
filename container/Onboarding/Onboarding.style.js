b3fab290724f7e23c3744e794b9f42029edacee6
Walcr <walcr.com@gmail.com>
2024-07-04 20:48:06 +0100

    Update Onboarding.style.js

--- container/Onboarding/Onboarding.style.js
+++ container/Onboarding/Onboarding.style.js
@@ -7,7 +7,7 @@ export const OnboardingStyle = styled.div`
   height: 100dvh;
 
   .header {
-    padding: 3%;
+    padding: 6%;
     display: flex;
     justify-content: space-between;
     align-items: center;
