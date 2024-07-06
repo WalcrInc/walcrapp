1724f2c7759f4e87bdc1f07e98b8398ac3533aef
Ay7ot <ayomidotzee@gmail.com>
2024-07-06 05:49:07 -0700

    Added and moved the paddings on dashboard pages

--- components/BottomNavbar/BottomNav.style.js
+++ components/BottomNavbar/BottomNav.style.js
@@ -1,16 +1,14 @@
 import styled from "styled-components";
 
-
 export const Container = styled.div`
-display:flex ;
-justify-content:space-between ;
-align-items:end ;
-padding:3% 5% ;
-position:fixed ;
-bottom: 0;
-width:100% ;
-margin:0 auto ;
-left:0 ;
-background: #FFFFFF;
-
-`
+  display: flex;
+  justify-content: space-between;
+  align-items: end;
+  padding: 3rem 5%;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  margin: 0 auto;
+  left: 0;
+  background: #ffffff;
+`;
