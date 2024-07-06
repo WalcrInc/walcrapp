f956f7ae4a8a0a3301e5f5febdeb3cf102089a9b
Ay7ot <ayomidotzee@gmail.com>
2024-07-06 05:29:01 -0700

    Added back style override to _document.js file

--- pages/_document.js
+++ pages/_document.js
@@ -7,11 +7,14 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="manifest" href="/manifest.json" />
-          {/* <link rel="stylesheet" href="styleovrride.css" />  */}
+          <link rel="stylesheet" href="styleovrride.css" />
           <meta name="mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover"
+          />
         </Head>
         <body>
           <Main />
