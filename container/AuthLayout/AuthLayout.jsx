553f01fc6750eba553a1afe9fbb677684ecc9478
Aydot <ayomidotzee@gmail.com>
2024-07-10 07:20:49 +0000

    Fix 1 for signup page overflow on status bar

--- container/AuthLayout/AuthLayout.jsx
+++ container/AuthLayout/AuthLayout.jsx
@@ -2,7 +2,19 @@ import React from "react";
 import { AuthStyle } from "./AuthLayout.style";
 
 const AuthLayout = ({ children }) => {
-  return <AuthStyle>{children}</AuthStyle>;
+  return (
+    <div
+      style={{
+        height: "100dvh",
+        padding: "0rem 0 3rem",
+        overflow: "hidden",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <AuthStyle>{children}</AuthStyle>
+    </div>
+  );
 };
 
 export default AuthLayout;
--- pages/login.js
+++ pages/login.js
@@ -4,19 +4,9 @@ import React from "react";
 
 const login = () => {
   return (
-    <div
-      style={{
-        height: "100dvh",
-        padding: "0rem 0 3rem",
-        overflow: "hidden",
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-      <AuthLayout>
-        <Login />
-      </AuthLayout>
-    </div>
+    <AuthLayout>
+      <Login />
+    </AuthLayout>
   );
 };
 
