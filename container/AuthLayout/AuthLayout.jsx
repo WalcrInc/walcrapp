3cdc019ce47f0652a1a232c70ce6e086ce407583
Aydot <ayomidotzee@gmail.com>
2024-07-10 07:32:17 +0000

    Fix 2 for scroll on sign in page

--- container/AuthLayout/AuthLayout.jsx
+++ container/AuthLayout/AuthLayout.jsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { AuthStyle } from "./AuthLayout.style";
 
 const AuthLayout = ({ children }) => {
+  useEffect(() => {
+    const preventDefault = (e) => e.preventDefault();
+
+    // Add the class to the body to prevent scrolling
+    document.body.classList.add("no-scroll");
+
+    // Add event listeners to prevent touch and drag on the body
+    document.body.addEventListener('touchmove', preventDefault, { passive: false });
+    document.body.addEventListener('wheel', preventDefault, { passive: false });
+
+    // Cleanup function to remove the class and event listeners
+    return () => {
+      document.body.classList.remove("no-scroll");
+      document.body.removeEventListener('touchmove', preventDefault);
+      document.body.removeEventListener('wheel', preventDefault);
+    };
+  }, []);
+
   return (
     <div
       style={{
         height: "100dvh",
         padding: "0rem 0 3rem",
-        overflow: "scroll",
+        overflowY: "auto", // Allow vertical scrolling inside this div
         display: "flex",
         flexDirection: "column",
         overscrollBehavior: "none"
